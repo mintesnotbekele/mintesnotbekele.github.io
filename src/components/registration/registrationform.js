b5d355195984ccf83fe7d559e543f1057932f995
Mintesnot Bekele <mintesnotbekele23@gmail.com>
2022-09-01 17:11:21 +0300

    github deploy properties

--- src/components/registration/registrationform.js
+++ src/components/registration/registrationform.js
@@ -52,6 +52,14 @@ const RegistrationForm =()=>{
                         <option>No</option>
                        </Select>
                 </div>
+        
+                <div class="flex w-full">
+                <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 rounded-l-md border border-r-0 border-gray-300 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path></svg>
+                    </span>
+                <TextInput type="text" id="website-admin" class="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="091580097--"/>
+                </div>
+            
                 <div className="md-2 block">
                 <Button type="submit" color="purple">
                     Register
@@ -67,7 +75,7 @@ export default RegistrationForm;
 
 export const RegistrationHeader=()=>{
    return (
-    <><div className="text-center text-4xl font-sans mt-20 bg-gray-400 w-1/3 mx-auto">
+    <><div className="text-center rounded-lg py-2 text-4xl font-sans mt-20 bg-gray-400 w-1/3 mx-auto">
         New member registration
         </div></>
    )
