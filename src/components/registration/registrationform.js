71bfdbedb3b0b57d3639d06a7e9dbbd18c0779a4
Mintesnot Bekele <mintesnotbekele23@gmail.com>
2022-09-02 18:30:14 +0300

    test deploy

--- src/components/registration/registrationform.js
+++ src/components/registration/registrationform.js
@@ -52,7 +52,7 @@ const RegistrationForm =()=>{
                         <option>No</option>
                        </Select>
                 </div>
-        
+         
                 <div class="flex w-full">
                 <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 rounded-l-md border border-r-0 border-gray-300 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
                 <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path></svg>
